c32de6232cf2f73e41735fb88e294ef058b0949f
Wil Gieseler <supapuerco@gmail.com>
2012-04-13 22:15:53 -0700

    Initial Commit

---
+++ Trenchrun/MocoDocument.h
@@ -0,0 +1,13 @@
+//
+//  MocoDocument.h
+//  Trenchrun
+//
+//  Created by Wil Gieseler on 4/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MocoDocument : NSDocument
+
+@end
---
+++ Trenchrun/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Trenchrun
+//
+//  Created by Wil Gieseler on 4/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
