29c8037dc6da4eff76994abdbc689ca2c9e9f34b
Wil Gieseler <supapuerco@gmail.com>
2012-04-22 00:00:54 -0700

    Tried to add release notes. Didn't.

---
+++ Trenchrun/ReleaseNotesWindowController.m
@@ -0,0 +1,34 @@
+//
+//  ReleaseNotesWindowController.m
+//  Trenchrun
+//
+//  Created by Wil Gieseler on 4/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ReleaseNotesWindowController.h"
+
+@interface ReleaseNotesWindowController ()
+
+@end
+
+@implementation ReleaseNotesWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
