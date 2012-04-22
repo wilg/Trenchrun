cec4da586d5c90fb76ec6646b4cc16862df82738
Wil Gieseler <supapuerco@gmail.com>
2012-04-22 00:00:54 -0700

    Tried to add release notes. Didn't.

---
+++ Trenchrun/ReleaseNotesWindowController.h
@@ -0,0 +1,13 @@
+//
+//  ReleaseNotesWindowController.h
+//  Trenchrun
+//
+//  Created by Wil Gieseler on 4/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ReleaseNotesWindowController : NSWindowController
+
+@end
