ad6b4f383c0b0c6e799bf68ae21dcb043d55bf60
Wil Gieseler <supapuerco@gmail.com>
2012-04-21 20:55:58 -0700

    Protocol update

---
+++ Trenchrun/MocoAxisPosition.h
@@ -0,0 +1,13 @@
+//
+//  MocoAxisPosition.h
+//  Trenchrun
+//
+//  Created by Wil Gieseler on 4/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MocoAxisPosition : NSObject
+
+@end
---
+++ Trenchrun/MocoAxisPosition.m
@@ -0,0 +1,13 @@
+//
+//  MocoAxisPosition.m
+//  Trenchrun
+//
+//  Created by Wil Gieseler on 4/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MocoAxisPosition.h"
+
+@implementation MocoAxisPosition
+
+@end
