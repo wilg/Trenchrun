2c20cad4310dcefc231e125bcb5528883758e728
Wil Gieseler <supapuerco@gmail.com>
2012-04-22 03:27:11 -0700

    Cleaned up AppDelegate

--- Trenchrun/MocoAppDelegate.m
+++ Trenchrun/MocoAppDelegate.m
@@ -13,48 +13,10 @@
 
 @implementation MocoAppDelegate
 
-@synthesize window = _window;
-@synthesize panWheel;
-@synthesize tiltWheel;
-@synthesize panText;
-@synthesize tiltText;
 @synthesize mocoDriver;
-
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(thingMoved:) name:@"MocoDriverDidRecieveInput" object:nil];
-    
-//    mocoDriver = [[MocoDriver alloc] init];
-
-}
 
 - (void)applicationWillTerminate:(NSNotification *)notification {
     [mocoDriver severConnections];
 }
-
-- (float)normalizedPercentageFromEncoderValue:(int)encoderValue {
-    return (float)(encoderValue % 4096) / 4096.f;
-}
-
-
-- (void)thingMoved:(NSNotification *)notification {
-    
-    NSLog(@"notification object: %@", notification.object);
-    
-//    float panPercent = [self normalizedPercentageFromEncoderValue:[[notification.object objectAtIndex:0] intValue]];
-//    float tiltPercent = [self normalizedPercentageFromEncoderValue:[[notification.object objectAtIndex:1] intValue]];
-//
-//    
-//    
-//    self.panText.stringValue = [NSString stringWithFormat:@"%f", panPercent];
-//    self.tiltText.stringValue = [NSString stringWithFormat:@"%f", tiltPercent];
-//    
-//    [self.panWheel setDoubleValue:panPercent * 100.0];
-//    
-//    [self.tiltWheel setDoubleValue:tiltPercent * 100.0];
-//
-    
-}
-
 
 @end
