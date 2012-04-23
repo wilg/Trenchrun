d8b6a8dc321036c486650c14e326d7c7b897b6cd
Wil Gieseler <supapuerco@gmail.com>
2012-04-22 20:40:12 -0700

    Added highly bullshit fps input window and some other bogus shit

---
+++ Trenchrun/MocoAxisVisualizationView.h
@@ -0,0 +1,16 @@
+//
+//  MocoAxisVisualizationView.h
+//  Trenchrun
+//
+//  Created by Wil Gieseler on 4/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "MocoProtocolConstants.h"
+
+@interface MocoAxisVisualizationView : NSView
+@property MocoAxis axis;
+@property double position;
+
+@end
